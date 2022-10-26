b96489955ad36af0d186925155663f0c33e9689f
Shuvrajeet Das <shuvrajeet17@gmail.com>
2022-10-26 18:22:09 +0530

    updates

--- ConvolutionalNeuralNetwork/ResNet/resnet.py
+++ ConvolutionalNeuralNetwork/ResNet/resnet.py
@@ -10,7 +10,7 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 callbacks = [
     tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=7),
     tf.keras.callbacks.ModelCheckpoint(
-        filepath='model_alexnet.h5', save_weights_only=True, monitor='val_loss', save_best_only=True),
+        filepath='model_res_net.h5', save_weights_only=True, monitor='val_loss', save_best_only=True),
     tf.keras.callbacks.TensorBoard(
         log_dir='./logs', histogram_freq=1, write_graph=True),
     tf.keras.callbacks.ReduceLROnPlateau(
