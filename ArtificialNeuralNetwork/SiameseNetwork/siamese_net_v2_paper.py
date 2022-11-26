28e612b34065c70607bfcf60136450a9c4cc5239
Shuvrajeet Das <shuvrajeet17@gmail.com>
2022-11-26 19:16:12 +0530

    Update siamese_net_v2_paper.py

--- ArtificialNeuralNetwork/SiameseNetwork/siamese_net_v2_paper.py
+++ ArtificialNeuralNetwork/SiameseNetwork/siamese_net_v2_paper.py
@@ -9,7 +9,9 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 def euclidean_distance(vects):
     x, y = vects
-    return tf.keras.backend.sqrt(tf.keras.backend.sum(tf.keras.backend.square(x - y), axis=1, keepdims=True))
+    x = tf.cast(x, tf.float32)
+    y = tf.cast(y, tf.float32)
+    return tf.math.sqrt(tf.reduce_sum(tf.math.square(x - y), axis=1, keepdims=True))
 
 
 def eucl_dist_output_shape(shapes):
@@ -18,9 +20,7 @@ def eucl_dist_output_shape(shapes):
 
 
 def contrastive_loss(y_true, y_pred):
-    margin = 1
-    return tf.keras.backend.mean(y_true * tf.keras.backend.square(y_pred) + (1 - y_true) * tf.keras.backend.square(tf.keras.backend.maximum(margin - y_pred, 0)))
-
+    return tf.keras.backend.mean(y_true * tf.keras.backend.square(y_pred) + (1 - y_true) * tf.keras.backend.square(1 - y_pred))
 
 def create_pairs(x, digit_indices):
     pairs = []
@@ -47,11 +47,11 @@ def compute_accuracy(predictions, labels):
 def create_base_network_dense(input_dim):
     seq = tf.keras.models.Sequential()
     seq.add(tf.keras.layers.Dense(
-        128, input_shape=(input_dim,), activation='relu'))
+        32, input_shape=input_dim, activation='relu'))
     seq.add(tf.keras.layers.Dropout(0.1))
-    seq.add(tf.keras.layers.Dense(128, activation='relu'))
+    seq.add(tf.keras.layers.Dense(32, activation='relu'))
     seq.add(tf.keras.layers.Dropout(0.1))
-    seq.add(tf.keras.layers.Dense(128, activation='relu'))
+    seq.add(tf.keras.layers.Dense(32, activation='relu'))
     return seq
 
 
@@ -62,7 +62,7 @@ def create_base_network(input_dim):
     nb_conv = 3
     model = tf.keras.models.Sequential()
     model.add(tf.keras.layers.Conv2D(nb_filters, nb_conv, nb_conv,
-                                     border_mode='valid',
+                                     padding='valid',
                                      input_shape=(img_colours, img_rows, img_cols)))
     model.add(tf.keras.layers.Activation('relu'))
     model.add(tf.keras.layers.Conv2D(nb_filters, nb_conv, nb_conv))
@@ -79,10 +79,7 @@ def create_base_network(input_dim):
 
 
 if __name__ == '__main__':
-    # the data, shuffled and split between train and test sets
     (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
-    #X_train = X_train.reshape(60000, 784)
-    #X_test = X_test.reshape(10000, 784)
 
     # input image dimensions
     img_rows, img_cols = 28, 28
@@ -91,6 +88,8 @@ if __name__ == '__main__':
 
     X_train = X_train.astype('float32')
     X_test = X_test.astype('float32')
+    y_train = y_train.astype('float32')
+    y_test = y_test.astype('float32')
     X_train /= 255
     X_test /= 255
     #input_dim = 784
@@ -100,10 +99,12 @@ if __name__ == '__main__':
     # create training+test positive and negative pairs
     digit_indices = [np.where(y_train == i)[0] for i in range(10)]
     tr_pairs, tr_y = create_pairs(X_train, digit_indices)
+    tr_y = tf.cast(tr_y, tf.float32)
 
     digit_indices = [np.where(y_test == i)[0] for i in range(10)]
     te_pairs, te_y = create_pairs(X_test, digit_indices)
-    base_network = create_base_network(input_dim)
+    te_y = tf.cast(te_y, tf.float32)
+    base_network = create_base_network_dense(input_dim)
 
     input_a = tf.keras.layers.Input(shape=(1, img_rows, img_cols,))
     input_b = tf.keras.layers.Input(shape=(1, img_rows, img_cols,))
@@ -114,7 +115,7 @@ if __name__ == '__main__':
     distance = tf.keras.layers.Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)(
         [processed_a, processed_b])
 
-    model = tf.keras.Model(input=[input_a, input_b], output=distance)
+    model = tf.keras.Model(inputs=[input_a, input_b], outputs=distance)
 
     # train
     rms = tf.keras.optimizers.RMSprop()
@@ -122,8 +123,7 @@ if __name__ == '__main__':
     model.fit([tr_pairs[:, 0], tr_pairs[:, 1]], tr_y,
               validation_data=([te_pairs[:, 0], te_pairs[:, 1]], te_y),
               batch_size=128,
-              nb_epoch=nb_epoch)
-    #model.fit_generator(myGenerator(), samples_per_epoch = 60000, nb_epoch = 2, verbose=2, show_accuracy=True, callbacks=[], validation_data=None, class_weight=None, nb_worker=1)
+              epochs=nb_epoch)
 
     # compute final accuracy on training and test sets
     pred = model.predict([tr_pairs[:, 0], tr_pairs[:, 1]])
